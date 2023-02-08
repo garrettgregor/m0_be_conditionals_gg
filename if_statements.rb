bcad678fd079dbb4d6e0ec921c69ea2414334707
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-08 08:56:11 -0700

    Add if_statements homework

--- if_statements.rb
+++ if_statements.rb
@@ -41,10 +41,19 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 3
+cost_gumball_quarters = 2
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+if num_quarters < 2
+  puts "I don't have enough money for a gumball"
+elsif num_quarters >= 2
+  puts "I have enough money for a gumball"
+else
+  puts "Do you even want a guball?"
+end
+
+# puts "I have enough money for a gumball"
+# puts "I don't have enough money for a gumball"
 
 
 #####################
@@ -61,5 +70,11 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour >= 2 && has_sauce == true
+  p "I can make a pizza"
+else cups_of_flour < 2 && has_sauce == false
+  p "I cannot make a pizza"
+end
