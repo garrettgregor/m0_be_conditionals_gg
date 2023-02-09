d33f451b0055648c543b9ef019ab9981b3b8dbd6
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-09 12:23:11 -0700

    Add code challenge 3 with a twist

---
+++ code_challenges/code_challenge3.rb
@@ -0,0 +1,23 @@
+# ## Spicy
+
+# Write a Ruby program that prints out a String or Integer: 
+# The printed value will depend on the value of an Integer. 
+# If the Integer is a multiple of 3, print "Fizz". 
+# If the Integer is a multiple of 5, print "Buzz". 
+# If the Integer is a multiple of both 3 and 5, print "FizzBuzz". 
+# If the Integer is not a multiple of either, print the Integer itself.
+
+puts "What's your number?"
+integer = gets.chomp.to_i
+
+    if  integer == 0
+        puts "#{integer}"
+        elsif integer % 3 == 0 && integer % 5 == 0
+                puts "Fizzbuzz"
+            elsif integer % 3 == 0
+                puts "Fizz"
+                elsif integer % 5 == 0
+                    puts "Buzz"
+                    else
+                        puts "#{integer}"
+    end
