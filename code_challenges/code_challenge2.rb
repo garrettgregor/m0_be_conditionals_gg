d769dc49e2a321a1eed53882dff4d4e99825a1ba
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-11 11:24:28 -0700

    Revise code challenge 2

--- code_challenges/code_challenge2.rb
+++ code_challenges/code_challenge2.rb
@@ -12,15 +12,13 @@
 # - If the user either has a good record or is over 25 years old, they should pay full price
 # - If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
 
-good_driving_record = true
-age = 26
+good_driving_record = false
+age = 20
 
 if good_driving_record == true && age >= 25
     puts "You get a discount on your car rental!"
-elsif good_driving_record == false || age >= 25
+elsif good_driving_record == true || age >= 25
     puts "You need to pay full price"
-elsif good_driving_record == false && age < 25
-    puts "You need to have someone else sign for the rental"  
 else
-    puts "You can't rent this car"
+    puts "You need to have someone else sign for the rental"  
 end
